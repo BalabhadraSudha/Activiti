25a324a592f031881a50dab827fe45d145a8345d
Vasile Dirla <vasile@dirla.ro>
2017-04-25 09:54:57 +0300

    increased timeout

--- modules/activiti-engine/src/test/java/org/activiti/engine/test/api/event/JobEventsTest.java
+++ modules/activiti-engine/src/test/java/org/activiti/engine/test/api/event/JobEventsTest.java
@@ -320,7 +320,7 @@ public class JobEventsTest extends PluggableActivitiTestCase {
     Calendar tomorrow = Calendar.getInstance();
     tomorrow.add(Calendar.DAY_OF_YEAR, 1);
     processEngineConfiguration.getClock().setCurrentTime(tomorrow.getTime());
-    waitForJobExecutorToProcessAllJobs(2000, 100);
+    waitForJobExecutorToProcessAllJobs(10000, 100);
 
     checkEventCount(1, ActivitiEventType.TIMER_SCHEDULED);
     checkEventCount(0, ActivitiEventType.JOB_CANCELED);
